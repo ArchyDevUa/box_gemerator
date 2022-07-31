c346e911dabfda640fc3afc502d1f4de80ddb628
ArchyDev <archydevua@gmail.com>
2022-07-31 12:09:51 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Box from './Box';
 import './App.css';
 
 function App() {
+  const [posts,setPosts] = useState([1,1,1]);
+
+  function hadlerAddBlock(){
+    setPosts([...posts,1])
+    console.log(posts)
+  }
+
+  function hadlerDeleteBlock(){
+    setPosts(posts.slice(0,-1))
+    console.log(posts)
+  }
+
+  function hadlerAddBlock(){
+    setPosts([...posts,1])
+    console.log(posts)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={hadlerAddBlock}> + </button>
+      <button onClick={hadlerDeleteBlock}> - </button>
+      {(posts.map((e,index) => (
+        <Box key={index}/>
+      ))
+      )}
     </div>
   );
 }
---
+++ src/Box.js
@@ -0,0 +1,12 @@
+function Box(props){
+    return(
+        <div style={{
+            width:"200px",
+            height:"50px",
+            border:"3px solid black",
+            margin:"10px auto"
+        }}>Block</div>
+    )
+}
+
+export default Box;
